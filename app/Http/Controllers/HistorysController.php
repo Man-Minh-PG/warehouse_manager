aefa8b64d4d2af1b6d51e141a222f94d60ddd532
Man-Minh-PG <manbui0207@gmail.com>
2023-01-13 16:54:02 +0700

    Process_route_page

---
+++ app/Http/Controllers/HistorysController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\acccount_historys;
+use App\Models\products;
+use App\Models\product_types;
+
+use App\Http\Requests\Storeacccount_historysRequest;
+use App\Http\Requests\Updateacccount_historysRequest;
+
+class HistorysController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\Storeacccount_historysRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Storeacccount_historysRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\acccount_historys  $acccount_historys
+     * @return \Illuminate\Http\Response
+     */
+    public function show(acccount_historys $acccount_historys)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\acccount_historys  $acccount_historys
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(acccount_historys $acccount_historys)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Updateacccount_historysRequest  $request
+     * @param  \App\Models\acccount_historys  $acccount_historys
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Updateacccount_historysRequest $request, acccount_historys $acccount_historys)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\acccount_historys  $acccount_historys
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(acccount_historys $acccount_historys)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,16 +20,54 @@ Route::group(['prefix' => '/', 'controller' => HomeController::class, 'middlewar
     Route::get('/', 'index')->name('admin.home');
 });
 
+/*Account*/
 Route::group(['controller' => UserController::class], function(){
     Route::get('/login', 'index')->name('login');
     Route::post('/login', 'show')->name('show_login');
     Route::get('/logout', 'logout')->name('logout');
 });
 
+/*Home Page*/
+Route::group(['prefix' => '_admin/home', 'controller' => HomeController::class], function(){
+    //Return view
+    Route::get('/', 'index')->name('admin.home');
+});
+
+/*Product*/ 
+Route::group(['prefix' => '_admin/product', 'controller' => ProductsController::class], function(){
+    //Return view
+    Route::get('/', 'index')->name('admin.home');
+    Route::get('/create', 'index')->name('admin.home.create');
+    //Process create - update 
+
+    //Process Delete
+
+    
+});
+
+/*Product Type*/ 
+Route::group(['prefix' => '_admin/product_type', 'controller' => ProductTypesController::class], function(){
+    Route::get('/', 'index')->name('admin.home');
+});
+
+/*Unit Code*/ 
+Route::group(['prefix' => '_admin/unit', 'controller' => UnitCodes::class], function(){
+    Route::get('/', 'index')->name('admin.home');
+});
+
+/*Warehouse*/ 
+Route::group(['prefix' => '_admin/warehouse', 'controller' => WarehouseController::class], function(){
+    Route::get('/', 'index')->name('admin.home');
+});
+
+/*History*/ 
+Route::group(['prefix' => '_admin/history', 'controller' => HistorysController::class], function(){
+    Route::get('/', 'index')->name('admin.home');
+});
+
 // Route::group(['prefix' => '_admin/home', 'controller' => HomeController::class, 'middleware' => 'auth'], function(){
 //     Route::get('/', 'index')->name('admin.home');
 // });
 
-Route::group(['prefix' => '_admin/home', 'controller' => HomeController::class], function(){
-    Route::get('/', 'index')->name('admin.home');
-});
+
+/*Xem xet den truong hop*/
